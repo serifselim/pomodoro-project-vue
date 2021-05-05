d2bcd785c097b13d3ce2b1d44835e72f3dfd3cf2
Your Name <selimserifoglu03@gmail.com>
2021-05-05 18:52:15 +0300

    counter's style finished

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,13 +1,23 @@
 <template>
-  <section class="counter">
-    <div class="counter__left-arrow arrow"><i class="fas fa-angle-double-left fa-7x"></i></div>
-    <div class="counter__main">
-      <div class="counter__main--count">
-        <span>25:00</span>
+  <section class="main-counter">
+      
+    <div class="counter">
+      <div class="counter__left-arrow arrow">
+        <i class="fas fa-angle-double-left fa-7x"></i>
+      </div>
+      <div class="counter__main">
+        <div class="counter__main--count">
+          <span>25:00</span>
+        </div>
+      </div>
+      <div class="counter__right-arrow arrow">
+        <i class="fas fa-angle-double-right fa-7x"></i>
       </div>
     </div>
-    <div class="counter__right-arrow arrow">
-        <i class="fas fa-angle-double-right fa-7x"></i>
+
+    <div class="start">
+      <button class="start__btn">start</button>
     </div>
+
   </section>
 </template>
