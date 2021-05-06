05487a316e744b60472e608d6c294c321b7f9d0f
Your Name <selimserifoglu03@gmail.com>
2021-05-06 21:29:00 +0300

    timer mechanic added

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,6 +1,5 @@
 <template>
   <section class="main-counter">
-      
     <div class="counter">
       <div class="timeout">
         <button class="timeout-btn">pomodoro</button>
@@ -9,28 +8,56 @@
       </div>
       <div class="main">
         <div class="count">
-          <span>25:00</span>
+          <span>{{ pMText }}:{{ pSText }}</span>
         </div>
       </div>
     </div>
     <div class="start">
-      <button  @click="startPomo" class="start-btn">start</button>
+      <button v-if="true" @click="startPomo" class="pomo-btn">start</button>
+      <button  @click="stopPomo" class="pomo-btn">stop</button>
     </div>
   </section>
 </template>
 
 <script>
 export default {
-  name:'Counter',
-  data(){
-    return{
-      hello : this.$store.state.hello
-    }
+  name: "Counter",
+  data() {
+    return {
+      pM: 5,
+      pS: 0,
+      pMText: 25,
+      pSText: "00",
+      Interval: null,
+    };
   },
-  methods : {
-    startPomo(){
-      this.$store.dispatch('helloWorld');
-    }
-  }
-}
+  methods: {
+    startPomo() {
+      this.Interval = setInterval(this.pomoTimer, 100);
+    },
+
+    stopPomo(){
+      clearInterval(this.Interval);
+    },
+
+    pomoTimer() {
+      
+      if (this.pS <= 0) {
+        this.pM--;
+        this.pM < 10 ? this.pMText = "0" + this.pM : this.pMText = this.pM;
+
+        this.pS = 59;
+        this.pSText = this.pS;
+      }else{
+        this.pS--;
+        this.pS < 10 ? this.pSText = "0" + this.pS : this.pSText = this.pS;
+      }
+      if(this.pM <= 0 && this.pS <= 0){
+        this.stopPomo();
+      }
+
+      
+    },
+  },
+};
 </script>
