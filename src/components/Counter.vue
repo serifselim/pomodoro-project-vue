90b279f03f005cc388520cdd98b6fd54ce3c6ab9
Your Name <selimserifoglu03@gmail.com>
2021-05-06 14:49:49 +0300

    deleted scss stlyes

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -2,22 +2,30 @@
   <section class="main-counter">
       
     <div class="counter">
-      <div class="counter__left-arrow arrow">
-        <i class="fas fa-angle-double-left fa-7x"></i>
+      <div class="timeout">
+        <button class="timeout-btn">pomodoro</button>
+        <button class="timeout-btn">short break</button>
+        <button class="timeout-btn">long break</button>
       </div>
-      <div class="counter__main">
-        <div class="counter__main--count">
+      <div class="main">
+        <div class="count">
           <span>25:00</span>
         </div>
       </div>
-      <div class="counter__right-arrow arrow">
-        <i class="fas fa-angle-double-right fa-7x"></i>
-      </div>
     </div>
-
     <div class="start">
-      <button class="start__btn">start</button>
+      <button  @click="changeColor" class="start-btn">start</button>
     </div>
-
   </section>
 </template>
+
+<script>
+export default {
+  name:'Counter',
+  methods : {
+    changeColor(){
+      document.querySelector('body').style.setProperty('--active-color', '#305e9b');
+    }
+  }
+}
+</script>
