c09845809e52fbec0d6fab7bfe189e899707c781
Your Name <selimserifoglu03@gmail.com>
2021-05-09 03:10:43 +0300

    pomodoro mechanic finished

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -13,8 +13,8 @@
       </div>
     </div>
     <div class="start">
-      <button v-if="true" @click="startPomo" class="pomo-btn">start</button>
-      <button  @click="stopPomo" class="pomo-btn">stop</button>
+      <button v-if="isStart" @click="startPomo" class="pomo-btn">start</button>
+      <button v-else @click="stopPomo" class="pomo-btn">stop</button>
     </div>
   </section>
 </template>
@@ -24,19 +24,25 @@ export default {
   name: "Counter",
   data() {
     return {
-      pM: 5,
+      pM: 10,
       pS: 0,
       pMText: 25,
       pSText: "00",
+      pCount : 0,
+      pFinishedCount : 0,
+      pSettings : [25,5,25,5,25,5,25,15],
       Interval: null,
+      isStart : true,
     };
   },
   methods: {
     startPomo() {
-      this.Interval = setInterval(this.pomoTimer, 100);
+      this.isStart = false;
+      this.Interval = setInterval(this.pomoTimer, 10);
     },
 
     stopPomo(){
+      this.isStart = true;
       clearInterval(this.Interval);
     },
 
@@ -53,11 +59,24 @@ export default {
         this.pS < 10 ? this.pSText = "0" + this.pS : this.pSText = this.pS;
       }
       if(this.pM <= 0 && this.pS <= 0){
+        this.isStart = true;
+        this.pCount < 7 ? this.pCount++ : this.pCount = 0;
+        this.nextStage(this.pCount);
         this.stopPomo();
       }
-
-      
     },
+    nextStage(pCount){
+      console.log("pCount" + pCount);
+      if (this.pSettings[pCount] === this.pSettings[0]) {
+        this.pFinishedCount++;
+      }
+      this.pM = this.pSettings[pCount];
+      this.pM < 10 ? this.pMText = "0" + this.pM : this.pMText = this.pM;
+    },
+
+    // zeroAddTime(time,text){
+    //   time < 10 ? text = "0" + time : text = time;
+    // }
   },
 };
 </script>
