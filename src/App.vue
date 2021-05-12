4e73aeba0c526f9f1cd3eb02462bd511b6b46c02
Your Name <selimserifoglu03@gmail.com>
2021-05-12 14:27:11 +0300

    counter deleted

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,21 @@
 <template>
   <div id="app">
     <Header/>
-    <Counter/>
+    <!-- <Counter/> -->
     <Todo/>
-
   </div>
 </template>
 
 <script>
 import Header from './components/Header';
-import Counter from './components/Counter';
+// import Counter from './components/Counter';
 import Todo from './components/Todo';
 
 
 export default {
   name: 'App',
   components: {
-    Header,Counter,Todo
+    Header,Todo
   }
 }
 </script>
